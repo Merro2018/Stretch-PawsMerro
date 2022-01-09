1824adb4eb74dff495da3df7d412b7353ce687b2
Matt Merrison <mattmerrison@gmail.com>
2022-01-09 14:00:42 +1100

    Initial Commit

---
+++ Stretch+Paws/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Stretch+Paws
+//
+//  Created by Matt on 3/1/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Purrfect!")
+            .font(.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Matt on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
